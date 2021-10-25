538506e37ed3dc9c10c2fbfbe85f725721cd3159
Ahmed Besbes <ahmedbe@rosafian.com>
2021-10-25 18:18:17 +0100

    Initial Commit

---
+++ MoviesHub/MoviesHub/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MoviesHub
+//
+//  Created by Ahmed Besbes on 25/10/2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController  = UINavigationController()
+        let initialViewController = MovieListViewController()
+        navigationController.viewControllers = [initialViewController]
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ MoviesHub/MoviesHub/Scenes/Movie List/MovieListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieListViewController.swift
+//  MoviesHub
+//
+//  Created by Ahmed Besbes on 25/10/2021.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
